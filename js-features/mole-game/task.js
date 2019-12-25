c4467c6b7c63af754e19eb991343b1f9062f53c7
Your Name <you@example.com>
2019-12-25 03:17:36 +0300

    first commit

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,33 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+
+for (let i = 1; i < 10; i++) {
+    let hole = document.getElementById(`hole${i}`);
+    function getHole(i) {
+        return hole
+    }
+    getHole(i);
+    hole.onclick = function () {
+        if (hole.className.includes('hole_has-mole') == true) {
+            dead.textContent++;
+        } else {
+            lost.textContent++;
+        }
+
+        if (dead.textContent == 10) {
+            alert ('Вы победили!');
+            dead.textContent = 0;
+            lost.textContent = 0;
+        }
+        
+        if (lost.textContent == 5) {
+            alert ('Вы проиграли');
+            dead.textContent = 0;
+            lost.textContent = 0;
+        }
+    }
+}
+
+    //Необходимо дописать игру, убивающую кротов при клике на них. В случайной лунке появляется крот. При нажатии на него, игроку засчитыва
+
+   // Случайным образом у одной из лунок добавляется класс hole_has-mole, что значит, что в лунке есть крот.
