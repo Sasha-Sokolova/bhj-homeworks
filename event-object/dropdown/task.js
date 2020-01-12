ebf328974e050abb8aeb005579f091d83fa18c3b
Your Name <you@example.com>
2020-01-12 08:38:16 +0300

    homework commit

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,69 @@
+const list = document.querySelector(".dropdown__list");
+const mainItem = document.querySelector(".dropdown__value");
+
+function getMenuItem() {
+    list.classList.add("dropdown__list_active");
+    return false
+}
+mainItem.addEventListener("click", getMenuItem);
+
+const menuItems = document.getElementsByClassName("dropdown__item");
+for (let i = 0; i < menuItems.length; i++) {
+    menuItems.item(i).onclick = function() {
+        mainItem.textContent = menuItems.item(i).textContent;
+        list.classList.remove("dropdown__list_active");
+        return false        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dropdown = document.querySelector(".dropdown__value");
+// const list = document.querySelector(".dropdown__list");
+
+// dropdown.onclick = function() {
+//     if (list.classList.contains("dropdown__list_active")) {
+//         list.classList.remove("dropdown__list_active");
+//     } else {
+//         list.classList.add("dropdown__list_active");
+//     }
+// }
+
+
+// const item = document.body.querySelectorAll(".dropdown__item");
+
+// for (let i = 0; i < item.length; i++) {
+//     item[i].addEventListener("click", function(event) {
+//         const er = this.closest('.dropdown__list');
+//         if (er.classList[0] == 'dropdown__list') {
+//             event.target.onclick = function () {
+//                 return false;
+//             }
+//             dropdownValue.textContent = event.target.textContent;
+//             dropdownList.classList.remove('dropdown__list_active');
+//         }
+//     });
+// }
+        
+    
+
+
+
+
--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -24,7 +24,18 @@ class Game {
       В случае правильного ввода слова вызываем this.success()
       При неправильном вводе символа - this.fail();
      */
+
+    document.addEventListener('keydown', (event)=>{
+			let symbol = this.currentSymbol;
+	        
+            if (symbol.textContent === event.key) {
+	            this.success();
+            } else {
+	            this.fail();
+            }
+        });
   }
+  
 
   success() {
     this.currentSymbol.classList.add('symbol_correct');
